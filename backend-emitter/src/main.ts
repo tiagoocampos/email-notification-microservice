e20f238b71d92d91b3f500b88b9ea1a398daa7c9
Tiago Campos <droiddzn68@gmail.com>
2026-08-09 23:18:17 -0300

    Deploy

--- backend-emitter/src/main.ts
+++ backend-emitter/src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   });
 
   await app.listen();
-  console.log('Email service escutando a fila....');
+  console.log('Email service escutando a fila...');
 }
 bootstrap();
